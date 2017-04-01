42c240049222bd3224f78495355debd346c24afd
nhtijotunn <nhtijotunn@gmail.com>
2017-04-01 07:33:27 -0400

    Map Style

--- MapWithMarker/app/src/main/java/com/example/mapwithmarker/MapsMarkerActivity.java
+++ MapWithMarker/app/src/main/java/com/example/mapwithmarker/MapsMarkerActivity.java
@@ -2,7 +2,10 @@ package com.example.mapwithmarker;
 
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,6 +14,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
+
 
 /**
  * An activity that displays a Google map with a marker (pin) to indicate a particular location.
@@ -18,13 +23,53 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsMarkerActivity extends AppCompatActivity
         implements OnMapReadyCallback {
 
+    private static final String TAG = MapsMarkerActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_maps);
+
+ /*       TabLayout tabLayout =
+                (TabLayout) findViewById(R.id.tab_layout);
+
+        final ViewPager viewPager =
+                (ViewPager) findViewById(R.id.map);
+        final PagerAdapter adapter = new TabPagerAdapter
+                (getSupportFragmentManager(),
+                        tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+
+        viewPager.addOnPageChangeListener(new
+                TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        tabLayout.addOnTabSelectedListener(new
+                                                   TabLayout.OnTabSelectedListener() {
+                                                       @Override
+                                                       public void onTabSelected(TabLayout.Tab tab) {
+                                                           viewPager.setCurrentItem(tab.getPosition());
+                                                       }
+
+                                                       @Override
+                                                       public void onTabUnselected(TabLayout.Tab tab) {
+
+                                                       }
+
+                                                       @Override
+                                                       public void onTabReselected(TabLayout.Tab tab) {
+
+                                                       }
+
+                                                   });
+
+
+*/
+
+
         // Get the SupportMapFragment and request notification
         // when the map is ready to be used.
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -42,6 +87,20 @@ public class MapsMarkerActivity extends AppCompatActivity
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+
+        // Customise the styling of the base map using a JSON object defined
+        // in a string resource file. First create a MapStyleOptions object
+        // from the JSON styles string, then pass this to the setMapStyle
+        // method of the GoogleMap object.
+        boolean success = googleMap.setMapStyle(new MapStyleOptions(getResources()
+                .getString(R.string.style_retro)));
+
+        if (!success) {
+            Log.e(TAG, "Style parsing failed.");
+        }
+
+
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
         LatLng nhti = new LatLng(43.223881, -71.531651);
@@ -57,6 +116,7 @@ public class MapsMarkerActivity extends AppCompatActivity
         googleMap.setLatLngBoundsForCameraTarget(campus);
 
     }
+
 
 
 }
